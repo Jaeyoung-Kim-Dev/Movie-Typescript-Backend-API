7ae065926d70002a3097488dccaa2e673bdc55ac
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-03-07 20:32:32 -0700

    Complete Unit test and E2E test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -12,7 +13,120 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  afterAll(() => {
+    // clear Database
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('should delete a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const allMoviesLength = service.getAll().length;
+      service.deleteOne(1);
+      const afterDeleteLength = service.getAll().length;
+      expect(afterDeleteLength).toBeLessThan(allMoviesLength);
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreateLength = service.getAll().length;
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const afterCreateLength = service.getAll().length;
+      expect(afterCreateLength).toBeGreaterThan(beforeCreateLength);
+      expect(service.getOne(1).title).toEqual('Test Movie');
+      expect(service.getOne(1).genres).toEqual(['test']);
+      expect(service.getOne(1).year).toEqual(2000);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const beforeUpdateLength = service.getAll().length;
+      service.update(1, {
+        title: 'Updated Movie',
+      });
+      const afterUpdateLength = service.getAll().length;
+      expect(afterUpdateLength).toEqual(beforeUpdateLength);
+      expect(service.getOne(1).title).toEqual('Updated Movie');
+      expect(service.getOne(1).genres).toEqual(['test']);
+      expect(service.getOne(1).year).toEqual(2000);
+
+      service.update(1, {
+        year: 2999,
+      });
+      expect(service.getOne(1).title).toEqual('Updated Movie');
+      expect(service.getOne(1).genres).toEqual(['test']);
+      expect(service.getOne(1).year).toEqual(2999);
+
+      service.update(1, {
+        genres: ['test', 'test2'],
+      });
+      expect(service.getOne(1).title).toEqual('Updated Movie');
+      expect(service.getOne(1).genres).toEqual(['test', 'test2']);
+      expect(service.getOne(1).year).toEqual(2999);
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.update(999, {
+          genres: ['test', 'test2'],
+        });
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,98 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    // changed it from beforeEach
+    // because it doesn't reset the DB. That means when a data is created in a test, it stays in the memory and can be used in another test.
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    // to make the same result with the real application. The pipes is added in the test.
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      })
+    );
+
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/ GET', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  describe('/movie', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()) //
+        .get('/movies')
+        .expect(200)
+        .expect([]);
+    });
+
+    it('POST 201', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test Movie',
+          genres: ['test'],
+          year: 2000,
+        })
+        .expect(201);
+    });
+
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test Movie',
+          genres: ['test'],
+          year: 2000,
+          other: 'Thing',
+        })
+        .expect(400);
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer()) //
+        .delete('/movies')
+        .expect(404);
+    });
+  });
+
+  describe('/movie/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated test' })
+        .expect(200);
+    });
+    it('PATCH 404', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/999')
+        .send({ title: 'Updated test' })
+        .expect(404);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
